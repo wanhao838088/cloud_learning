5205c1bdff06fc3d06657acc56d4beceef431165
wanhao <838088516@qq.com>
2019-09-18 15:54:49 +0800

    链路追踪

--- ConfigClient/src/main/java/com/wanhao/cloud/configclient/ConfigclientApplication.java
+++ ConfigClient/src/main/java/com/wanhao/cloud/configclient/ConfigclientApplication.java
@@ -3,11 +3,15 @@ package com.wanhao.cloud.configclient;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RefreshScope
+@EnableDiscoveryClient
 @EnableEurekaClient
 @SpringBootApplication
 public class ConfigclientApplication {
