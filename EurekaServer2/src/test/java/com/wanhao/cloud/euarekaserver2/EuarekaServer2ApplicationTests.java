f3d03e9871781ed00969e12f37996096ec4a4500
wanhao <838088516@qq.com>
2019-09-19 17:56:06 +0800

    eureka server高可用

---
+++ EurekaServer2/src/test/java/com/wanhao/cloud/euarekaserver2/EuarekaServer2ApplicationTests.java
@@ -0,0 +1,16 @@
+package com.wanhao.cloud.euarekaserver2;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class EuarekaServer2ApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
